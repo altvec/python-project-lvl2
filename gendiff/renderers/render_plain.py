86a4d606b663ef491017cdf6ec3b89855d5285ce
altvec <me@sergeykalistratov.com>
2019-08-03 12:32:00 +0500

    Removed unnecessary code

--- gendiff/renderers/render_plain.py
+++ gendiff/renderers/render_plain.py
@@ -14,27 +14,21 @@ def _get_changes(ast, parent=None, res=[]):
         if element['type'] == 'parent':
             _get_changes(element['child'], element['name'])
         if element['type'] == 'removed':
-            prop_name = _get_property(element, parent)
             res.append("Property '{prop}' was removed".format(
-                prop=prop_name,
+                prop=_get_property(element, parent),
             ))
         if element['type'] == 'added':
-            prop_name = _get_property(element, parent)
-            prop_value = _get_value(element['value'])
             res.append(
                 "Property '{prop}' was added with value: '{value}'".format(
-                    prop=prop_name,
-                    value=prop_value,
+                    prop=_get_property(element, parent),
+                    value=_get_value(element['value']),
                 ))
         if element['type'] == 'changed':
-            prop_name = _get_property(element, parent)
-            new_value = _get_value(element['new_value'])
-            old_value = _get_value(element['old_value'])
             res.append(
                 "Property '{prop}' was changed. From '{old}' to '{new}'".format(
-                    prop=prop_name,
-                    old=old_value,
-                    new=new_value,
+                    prop=_get_property(element, parent),
+                    old=_get_value(element['old_value']),
+                    new=_get_value(element['new_value']),
                 ))
     return res
 
