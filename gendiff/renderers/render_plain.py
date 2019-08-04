0ad0827ef0232b22b0fb85029ebb4eb3778fe8ce
altvec <me@sergeykalistratov.com>
2019-08-04 14:45:58 +0500

    Fixed plain renderer

--- gendiff/renderers/render_plain.py
+++ gendiff/renderers/render_plain.py
@@ -3,34 +3,32 @@
 """Plain render functions."""
 
 
-def render_plain(ast):
+def render_plain(ast, parent=None):
     """Render plain text."""
-    changes = _get_changes(ast)
-    return '\n'.join(changes)
+    changes = [
+        _get_changes(element, parent) for element in ast.values()
+    ]
+    return '\n'.join(filter(lambda element: element is not None, changes))
 
 
-def _get_changes(ast, parent=None, res=[]):
-    for element in ast.values():
-        if element['type'] == 'parent':
-            _get_changes(element['child'], element['name'])
-        if element['type'] == 'removed':
-            res.append("Property '{prop}' was removed".format(
-                prop=_get_property(element, parent),
-            ))
-        if element['type'] == 'added':
-            res.append(
-                "Property '{prop}' was added with value: '{value}'".format(
-                    prop=_get_property(element, parent),
-                    value=_get_value(element['value']),
-                ))
-        if element['type'] == 'changed':
-            res.append(
-                "Property '{prop}' was changed. From '{old}' to '{new}'".format(
-                    prop=_get_property(element, parent),
-                    old=_get_value(element['old_value']),
-                    new=_get_value(element['new_value']),
-                ))
-    return res
+def _get_changes(element, parent=None):
+    if element['type'] == 'parent':
+        return render_plain(element['child'], element['name'])
+    if element['type'] == 'removed':
+        return "Property '{prop}' was removed".format(
+            prop=_get_property(element, parent),
+        )
+    if element['type'] == 'added':
+        return "Property '{prop}' was added with value: '{value}'".format(
+            prop=_get_property(element, parent),
+            value=_get_value(element['value']),
+        )
+    if element['type'] == 'changed':
+        return "Property '{prop}' was changed. From '{old}' to '{new}'".format(
+            prop=_get_property(element, parent),
+            old=_get_value(element['old_value']),
+            new=_get_value(element['new_value']),
+        )
 
 
 def _get_value(prop_value):
