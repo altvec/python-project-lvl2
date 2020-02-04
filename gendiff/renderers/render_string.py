3cf551f6843eb8f49a183555f6d92dadd6d94d36
altvec <me@sergeykalistratov.com>
2020-02-05 00:28:09 +0500

    Simplified string render

--- gendiff/renderers/render_string.py
+++ gendiff/renderers/render_string.py
@@ -2,72 +2,72 @@
 
 """JSON-like string render functions."""
 
-from gendiff.constants import ADDED, CHANGED, PARENT, REMOVED, UNCHANGED
+from gendiff.constants import (
+    ADDED,
+    CHANGED,
+    INDENT,
+    NESTED,
+    REMOVED,
+    UNCHANGED,
+)
 
 
-def render_string(ast):
+def render_string(ast, depth=1):
     """Render pseudo JSON as string."""
-    changes = _get_changes(ast)
-    return '{{\n{changes}\n}}'.format(changes=changes)
+    diff = []
+    indent = INDENT * depth
+    for node_key, node_value in ast.items():
+        item_type = node_value.get('type')
+        item_value = node_value.get('value')
+        if item_type == NESTED:
+            diff.append('{indent}{state} {key}: {{'.format(
+                indent=indent,
+                state=UNCHANGED,
+                key=node_key,
+            ))
+            diff.append(render_string(item_value, depth + 2))
+            diff.append('{indent}}}'.format(indent=indent + INDENT))
+        elif item_type == CHANGED:
+            diff.append('{indent}{state} {key}: {value}'.format(
+                indent=indent,
+                state=ADDED,
+                key=node_key,
+                value=node_value.get('new_value'),
+            ))
+            diff.append('{indent}{state} {key}: {value}'.format(
+                indent=indent,
+                state=REMOVED,
+                key=node_key,
+                value=node_value.get('old_value'),
+            ))
+        else:
+            diff.append('{indent}{state} {key}: {value}'.format(
+                indent=indent,
+                state=item_type,
+                key=node_key,
+                value=_get_value(item_value, depth),
+            ))
+    if depth == 1:
+        diff = ['{'] + diff + ['}']
+    return '\n'.join(diff)
 
 
-def _get_changes(ast, depth=1):
-    indent = '  ' * depth
-    res = []
-    for element in ast.values():
-        element_type = element['type']
-        if element_type == PARENT:
-            change = '  {indent}{name}: {{\n{childs}\n  {indent}}}'.format(
-                indent=indent,
-                name=element['name'],
-                childs=_get_changes(element['child'], depth + 2),
-            )
-        if element_type == ADDED:
-            change = '{indent}+ {name}: {value}'.format(
-                indent=indent,
-                name=element['key'],
-                value=_get_value(element['value'], depth),
-            )
-        if element_type == REMOVED:
-            change = '{indent}- {name}: {value}'.format(
-                indent=indent,
-                name=element['key'],
-                value=_get_value(element['value'], depth),
-            )
-        if element_type == UNCHANGED:
-            change = '  {indent}{name}: {value}'.format(
-                indent=indent,
-                name=element['key'],
-                value=_get_value(element['value'], depth),
-            )
-        if element_type == CHANGED:
-            change = '{indent}+ {name}: {value}'.format(
-                indent=indent,
-                name=element['key'],
-                value=_get_value(element['new_value'], depth),
-            )
-            change += '\n{indent}- {name}: {value}'.format(
-                indent=indent,
-                name=element['key'],
-                value=_get_value(element['old_value'], depth),
-            )
-        res.append(change)
-    return '\n'.join(res)
-
-
-def _get_value(element, depth):
+def _get_value(element, depth=1):
     if isinstance(element, dict):
         return _render_array(element, depth)
     return element
 
 
 def _render_array(elements, depth):
-    indent = '  ' * depth
     res = []
+    res.append('{')
     for prop_name, prop_value in elements.items():
-        res.append('{{\n      {indent}{name}: {value}\n  {indent}}}'.format(
-            indent=indent,
+        res.append('{indent}{name}: {value}'.format(
+            indent=INDENT * (depth + 3),
             name=prop_name,
             value=prop_value,
+        ))
+        res.append('{indent}}}'.format(
+            indent=INDENT * (depth + 1),
         ))
     return '\n'.join(res)
