40557dd789a3d1b39976ae3a89186fd6617ab08d
altvec <me@sergeykalistratov.com>
2020-11-22 02:17:12 +0500

    minor fix

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-
 """Gendiff project."""
 
 from gendiff.generator import generate_diff
 
-__all__ = ['generate_diff']  # noqa: WPS410
+__all__ = ('generate_diff',)  # noqa: WPS410
